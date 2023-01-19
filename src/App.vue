0098868841048175a4cf6d422a5fdf5297e13ced
DarioGiusino <dario.giusino@gmail.com>
2023-01-19 15:35:32 +0100

    printed on page an image w/ AppHero component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
 import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue";
 export default {
   data() {
     return {};
   },
   components: {
     AppTitle,
+    AppHero,
   },
 };
 </script>
 
 <template>
   <AppTitle />
+  <AppHero />
 </template>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      pic: "banner.webp",
+    };
+  },
+  methods: {
+    buildUrlImage(image) {
+      const url = new URL(`./img/${image}`, import.meta.url);
+      return url.href;
+    },
+  },
+};
+</script>
+
+<template>
+  <img :src="buildUrlImage(pic)" alt="immagine" />
+</template>
+
+<style scoped>
+img {
+  width: 50%;
+}
+</style>
