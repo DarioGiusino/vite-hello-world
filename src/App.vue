f2b077328adb04bfc6c97bf5a9ab984aa3dbbee7
DarioGiusino <dario.giusino@gmail.com>
2023-01-19 15:21:38 +0100

    printed on page AppTitle component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script>
+import AppTitle from "./components/AppTitle.vue";
 export default {
   data() {
     return {};
   },
+  components: {
+    AppTitle,
+  },
 };
 </script>
+
+<template>
+  <AppTitle />
+</template>
